95e18da9d5d7592771922cd1c6299c2e7658f6e3
xingyue1111-ux <xingyue1111-ux@users.noreply.github.com>
2026-06-01 07:38:26 +0800

    chore: ignore local worktrees in lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,5 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 
 export default defineConfig([
   ...nextVitals,
-  globalIgnores([".next/**", "node_modules/**", "coverage/**"])
+  globalIgnores([".next/**", "node_modules/**", "coverage/**", ".worktrees/**"])
 ]);
